10c506fc083ac59ad758445d39dcf7e698e4dbe2
TIMMY <timotei.adam@emanuel.ro>
2023-11-27 09:51:36 +0200

    OOP - exercises 1

---
+++ OopExercises/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopExercises
+{
+    internal class Person
+    {
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age 
+        {
+            get
+            {
+                return DateTime.Now.Year - DateOfBirth.Year;
+            } 
+        }
+    }
+}
